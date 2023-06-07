3550962decac2e9144c88eee1d4b2f7c563a7610
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-06-07 14:42:46 +0200

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import usersRoute from "./routes/users.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+//in questo caso se si disconnette prova a connettersi di nuovo
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+//middlewares
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", usersRoute);
+app.use("/api/users", roomsRoute);
+
+//i parametri devono essere passati con questo ordine o non funzionerà
+app.use((err, req, res, next)=>{
+    const errorStauts = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStauts).json({
+        success : false,
+        status : errorStauts,
+        message: errorMessage,
+        stack: err.stack,
+    })
+})
+
+
+// app.get("/", (req, res)=>{
+//     res.send("hello first request")
+// })
+
+app.listen(8800, () => {
+  connect();
+  console.log("connected to back end");
+});
